f2d5fc6d071f97ec9dac7b867f2e178a05de90ab
Sahil Tara <supertfriend@gmail.com>
2019-07-16 20:17:52 -0400

    Added all commit data

--- src/components/Graphs/Bar/NumberVsNumberBarGraph/index.tsx
+++ src/components/Graphs/Bar/NumberVsNumberBarGraph/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   XYPlot,
   XAxis,
@@ -9,13 +9,13 @@ import {
   ChartLabel,
   VerticalBarSeriesPoint,
   FlexibleWidthXYPlot
-} from 'react-vis';
-import IBarGraphData from '../../../../types/IGraphData/IBarGraphData';
-import classNames from 'classnames';
+} from "react-vis";
+import IBarGraphData from "../../../../types/IGraphData/IBarGraphData";
+import classNames from "classnames";
 import styles from "./styles.module.css";
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 
-// takes in a title, a category, a list of {x, y, style?}, 
+// takes in a title, a category, a list of {x, y, style?},
 // and a maximum results to display (remaining are added to Other)
 interface IProps {
   title: string;
@@ -24,12 +24,20 @@ interface IProps {
   yAxisLabel: string;
 }
 
-const NumberVsNumberBarGraph: React.FC<IProps> = ({title, data, xAxisLabel, yAxisLabel}) => {
-  let maxX = 0, minX = 0, maxY = 0, minY = +Infinity;
-  const [state, setState] = useState({value: false});
+const NumberVsNumberBarGraph: React.FC<IProps> = ({
+  title,
+  data,
+  xAxisLabel,
+  yAxisLabel
+}) => {
+  let maxX = 0,
+    minX = 0,
+    maxY = 0,
+    minY = +Infinity;
+  const [state, setState] = useState({ value: false });
   const [tooltip, setTooltip] = useState({});
-  
-  data.forEach( (item: {x: number, y: number}) => {
+
+  data.forEach((item: { x: number; y: number }) => {
     if (item.x > maxX) {
       maxX = item.x;
     }
@@ -40,66 +48,69 @@ const NumberVsNumberBarGraph: React.FC<IProps> = ({title, data, xAxisLabel, yAxi
     }
   });
 
-  const yInterval = Math.ceil((maxY * 1.1)/4);
-  const yTicks = [0, yInterval, yInterval*2, yInterval*3, yInterval*4];
+  const yInterval = Math.ceil((maxY * 1.1) / 4);
+  const yTicks = [0, yInterval, yInterval * 2, yInterval * 3, yInterval * 4];
 
-  const xInterval = Math.ceil(maxX/4);
-  const xTicks = [minX, xInterval, xInterval*2, xInterval*3, xInterval*4];
+  const xInterval = Math.ceil(maxX / 4);
+  const xTicks = [minX, xInterval, xInterval * 2, xInterval * 3, xInterval * 4];
 
-  let mouseOver = (datapoint: VerticalBarSeriesPoint)=>{
-    setState({value: true});
-    setTooltip(xAxisLabel + ": " + datapoint.x + " • " + yAxisLabel + ": " + datapoint.y);
-  }
+  let mouseOver = (datapoint: VerticalBarSeriesPoint) => {
+    setState({ value: true });
+    setTooltip(
+      xAxisLabel + ": " + datapoint.x + " • " + yAxisLabel + ": " + datapoint.y
+    );
+  };
 
   return (
-    <Card style={{marginRight: "-10px", marginBottom: "20px"}}>
-      <h2 style={{paddingTop: "20px"}}>{title}</h2>
-        <FlexibleWidthXYPlot
-          xDomain={[minX, xTicks[4]]}
-          yDomain={[0, yTicks[4]]}
-          height={300}
-          margin={{left: 80, right: 50, bottom: 80}}
-        >
-          <VerticalGridLines />
-          <HorizontalGridLines />
+    <Card style={{ marginRight: "-10px", marginBottom: "20px" }}>
+      <h2 style={{ paddingTop: "20px" }}>{title}</h2>
+      <XYPlot
+        xDomain={[minX, xTicks[4]]}
+        yDomain={[0, yTicks[4]]}
+        width={500}
+        height={300}
+        margin={{ left: 80, right: 50, bottom: 80 }}
+      >
+        <VerticalGridLines />
+        <HorizontalGridLines />
 
-          <XAxis tickValues={xTicks} />
-          <ChartLabel
-            text={xAxisLabel}
-            className="alt-x-label"
-            includeMargin={true}
-            xPercent={0.48}
-            yPercent={0.64}
-          />
-          
-          <YAxis tickValues={yTicks} />
-          <ChartLabel
-            text={yAxisLabel}
-            className="alt-y-label"
-            includeMargin={true}
-            xPercent={0.05}
-            yPercent={0.06}
-            style={{
-              transform: 'rotate(-90)',
-              textAnchor: 'end'
-            }}
-          />
-
-        <VerticalBarSeries data={data} 
-          style={{stroke: "white"}}
-          onValueMouseOver={ datapoint => mouseOver(datapoint) }
+        <XAxis tickValues={xTicks} />
+        <ChartLabel
+          text={xAxisLabel}
+          className="alt-x-label"
+          includeMargin={true}
+          xPercent={0.48}
+          yPercent={0.64}
         />
-      </FlexibleWidthXYPlot>
-      {state.value? 
-        (<div className={classNames(styles.tooltip_box)}>
-            {tooltip}
-          </div>) : 
+
+        <YAxis tickValues={yTicks} />
+        <ChartLabel
+          text={yAxisLabel}
+          className="alt-y-label"
+          includeMargin={true}
+          xPercent={0.05}
+          yPercent={0.06}
+          style={{
+            transform: "rotate(-90)",
+            textAnchor: "end"
+          }}
+        />
+
+        <VerticalBarSeries
+          data={data}
+          style={{ stroke: "white" }}
+          onValueMouseOver={datapoint => mouseOver(datapoint)}
+        />
+      </XYPlot>
+      {state.value ? (
+        <div className={classNames(styles.tooltip_box)}>{tooltip}</div>
+      ) : (
         <div className={classNames(styles.tooltip_box)}>
           Hover over a bar for more information.
         </div>
-      }
+      )}
     </Card>
   );
-}
+};
 
 export default NumberVsNumberBarGraph;
--- src/containers/CommitsPage/index.tsx
+++ src/containers/CommitsPage/index.tsx
@@ -29,6 +29,7 @@ interface IDispatchProps {
 
 const commitLeaderboardInitial: IDonutGraphData[] = [];
 const commitsOverTimeInitial: IBarGraphData[] = [];
+const additionsAndDeletionsInitial: IBarGraphData[][] = [];
 
 type Props = IProps & IStateProps & IDispatchProps;
 
@@ -87,6 +88,45 @@ const getCommitsOverTimeData = (
   return commitsOverTimeGraphData;
 };
 
+const getAdditionsAndDeletionsData = (
+  commits: ICommitData[],
+  dateToCompare: Date
+): IBarGraphData[][] => {
+  const commitsAdditionDeletionProcessedData: Map<
+    number,
+    [number, number]
+  > = new Map();
+  const commitsAdditionGraphData: IBarGraphData[] = [];
+  const commitsDeletionGraphData: IBarGraphData[] = [];
+
+  commits.forEach(commit => {
+    const { createdAt, additions, deletions } = commit;
+    const date = new Date(createdAt);
+    date.setHours(0, 0, 0, 0);
+    const time = date.getTime();
+    if (time >= dateToCompare.getTime()) {
+      const additionsAndDeletionsForDate = commitsAdditionDeletionProcessedData.get(
+        time
+      );
+
+      if (additionsAndDeletionsForDate !== undefined) {
+        commitsAdditionDeletionProcessedData.set(time, [
+          additionsAndDeletionsForDate[0] + additions,
+          additionsAndDeletionsForDate[1] + deletions
+        ]);
+      } else {
+        commitsAdditionDeletionProcessedData.set(time, [additions, deletions]);
+      }
+    }
+  });
+  commitsAdditionDeletionProcessedData.forEach((value, key) => {
+    commitsAdditionGraphData.push({ x: key, y: value[0] });
+    commitsDeletionGraphData.push({ x: key, y: value[1] });
+  });
+
+  return [commitsAdditionGraphData, commitsDeletionGraphData];
+};
+
 const CommitsPage: React.FC<Props> = props => {
   const { commits, commitsMade, linesAdded, linesDeleted, timeSpan } = props;
 
@@ -96,6 +136,9 @@ const CommitsPage: React.FC<Props> = props => {
 
   const [commitsOverTime, setCommitsOverTime] = useState(
     commitsOverTimeInitial
+  );
+  const [additionsAndDeletions, setAdditionsAndDeletions] = useState(
+    additionsAndDeletionsInitial
   );
 
   useEffect(() => {
@@ -127,41 +170,17 @@ const CommitsPage: React.FC<Props> = props => {
     }
     setCommitLeaderBoard(getLeaderboardData(commits, timeToCompareTo));
     setCommitsOverTime(getCommitsOverTimeData(commits, timeToCompareTo));
+    setAdditionsAndDeletions(
+      getAdditionsAndDeletionsData(commits, timeToCompareTo)
+    );
   }, [commits, timeSpan]);
 
-  const lineDataAddition = [
-    { x: new Date("May 20 2017").getTime(), y: 1242 },
-    { x: new Date("May 23 2017").getTime(), y: 1217 },
-    { x: new Date("May 24 2017").getTime(), y: 1610 },
-    { x: new Date("May 26 2017").getTime(), y: 1963 },
-    { x: new Date("May 27 2017").getTime(), y: 1521 },
-    { x: new Date("May 28 2017").getTime(), y: 1423 },
-    { x: new Date("May 29 2017").getTime(), y: 1352 },
-    { x: new Date("May 30 2017").getTime(), y: 1777 },
-    { x: new Date("May 17 2017").getTime(), y: 1213 },
-    { x: new Date("May 16 2017").getTime(), y: 1016 },
-    { x: new Date("May 18 2017").getTime(), y: 1264 },
-    { x: new Date("May 13 2017").getTime(), y: 1186 }
-  ];
-
-  const lineDataDeletion = [
-    { x: new Date("May 20 2017").getTime(), y: 542 },
-    { x: new Date("May 23 2017").getTime(), y: 217 },
-    { x: new Date("May 24 2017").getTime(), y: 2670 },
-    { x: new Date("May 26 2017").getTime(), y: 963 },
-    { x: new Date("May 27 2017").getTime(), y: 1361 },
-    { x: new Date("May 28 2017").getTime(), y: 223 },
-    { x: new Date("May 29 2017").getTime(), y: 810 },
-    { x: new Date("May 30 2017").getTime(), y: 777 },
-    { x: new Date("May 17 2017").getTime(), y: 213 },
-    { x: new Date("May 16 2017").getTime(), y: 416 },
-    { x: new Date("May 18 2017").getTime(), y: 264 },
-    { x: new Date("May 13 2017").getTime(), y: 386 }
-  ];
-
   return (
-    <Container>
+    <Container className="show-grid">
       <div style={{ paddingTop: "20px" }}>
+        <Row>
+          <Col />
+        </Row>
         <Row>
           <Col>
             <Row>
@@ -183,7 +202,7 @@ const CommitsPage: React.FC<Props> = props => {
             />
             <NumberVsTimeMultiLineGraph
               title={"Additions and Deletions per Day"}
-              data={[lineDataAddition, lineDataDeletion]}
+              data={additionsAndDeletions}
               xAxisLabel={"Date"}
               yAxisLabel={"Lines"}
               lineLabels={["Additions", "Deletions"]}
