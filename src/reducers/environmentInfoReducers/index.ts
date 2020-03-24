5b013f61b799120f4b968a330ba57dfcf8cd70e0
Sahil Tara <supertfriend@gmail.com>
2020-03-24 18:50:56 -0400

    Added more comments, fixed slight bug with undefined/null values

--- src/reducers/environmentInfoReducers/index.ts
+++ src/reducers/environmentInfoReducers/index.ts
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import isLoggedIn from "./loginStatusReducer";
 
 /**
- * Environment Info root reducer
+ * Environment Info Root Reducer
  */
 const environmentInfoReducer = combineReducers({
   isLoggedIn
